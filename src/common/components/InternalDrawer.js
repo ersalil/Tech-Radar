d1004049d42faefde9f6311c9dc8f1fb8d693434
Salil Agrawal <salil.opera@gmail.com>
2024-10-18 14:09:18 +0530

    test

---
+++ src/common/components/InternalDrawer.js
@@ -0,0 +1,48 @@
+import React, { Fragment, useState } from "react";
+import { Drawer } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
+
+function InternalDrawer() {
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <Fragment>
+      <div onClick={showDrawer}>
+        <MenuOutlined className="drawer-icon" />
+      </div>
+      <Drawer
+        placement="right"
+        onClose={onClose}
+        open={open}
+        footer={
+          <div className="footer">
+            © {currentYear} Developed by Salil Agrawal All rights reserved.
+          </div>
+        }
+        footerStyle={{ padding: 0 }}
+      >
+        <div className="mobile-drawer">
+          {/* <img height={150} className="drawer-logo" src="/uprevollogo.png" alt="Sentioza" /> */}
+          <div className="drawer-links">
+            <a href="/">Search</a>
+            <a href="/techniques">Techniques</a>
+            <a href="/tools">Tools</a>
+            <a href="/platforms">Platforms</a>
+            <a href="/languagesandframeworks">Languages And Frameworks</a>
+          </div>
+        </div>
+      </Drawer>
+    </Fragment>
+  );
+}
+
+export default InternalDrawer;
